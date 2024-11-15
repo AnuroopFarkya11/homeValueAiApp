ac7c4e928b142521b77194ff073befac74432b61
AnuroopFarkya11 <anuroopf02@gmail.com>
2024-11-16 01:54:32 +0530

    Updated name and icon

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -14,7 +14,8 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData.light(useMaterial3: true),
+
       home: SplashScreen(),
     );
   }
